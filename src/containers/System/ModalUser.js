97e14489b1a80832b0f1438f7831d092a87b1db2
Nguyen Le <caonguyenle1810@gmail.com>
2022-04-17 10:57:13 +0700

    test

---
+++ src/containers/System/ModalUser.js
@@ -0,0 +1,130 @@
+import React, { Component } from "react";
+import { FormattedMessage } from "react-intl";
+import { Modal, ModalBody, ModalHeader, ModalFooter, Button } from "reactstrap";
+
+class ModalUser extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      firstName: "",
+      lastName: "",
+      address: "",
+    };
+  }
+
+  componentDidMount() {}
+
+  toggle = () => {
+    this.props.closeModal();
+  };
+
+  handleOnChangeInput(event) {
+    let inputName = event.target.name;
+    let inputValue = event.target.value;
+    this.setState({
+      [inputName]: inputValue,
+    });
+  }
+
+  checkValidInput = () => {
+    let isValid = true;
+    let data = { ...this.state };
+    for (const key in data) {
+      if (!data[key]) {
+        isValid = false;
+        alert(`Missing parameter: ${key}`);
+        break;
+      }
+    }
+    return isValid;
+  };
+
+  handleAddNewUser() {
+    let isValid = this.checkValidInput();
+    if (isValid) {
+      this.props.createNewUser(this.state);
+    }
+  }
+
+  render() {
+    // console.log(">>> check child props", this.props);
+    // console.log(">>> check child open modal", this.props.isOpen);
+    return (
+      <div>
+        <Modal
+          size="lg"
+          isOpen={this.props.isOpen}
+          className="model-user-container"
+          toggle={this.toggle}
+        >
+          <ModalHeader toggle={this.toggle}>Create a new user</ModalHeader>
+          <ModalBody>
+            <div className="modal-user-body">
+              <div className="input-container">
+                <label>Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  onChange={(event) => this.handleOnChangeInput(event)}
+                  value={this.state.email}
+                />
+              </div>
+              <div className="input-container">
+                <label>Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  onChange={(event) => this.handleOnChangeInput(event)}
+                  value={this.state.password}
+                />
+              </div>
+              <div className="input-container">
+                <label>First name</label>
+                <input
+                  type="text"
+                  name="firstName"
+                  onChange={(event) => this.handleOnChangeInput(event)}
+                  value={this.state.firstName}
+                />
+              </div>
+              <div className="input-container">
+                <label>Last Name</label>
+                <input
+                  type="text"
+                  name="lastName"
+                  onChange={(event) => this.handleOnChangeInput(event)}
+                  value={this.state.lastName}
+                />
+              </div>
+              <div className="input-container max-w">
+                <label>Address</label>
+                <input
+                  type="text"
+                  name="address"
+                  onChange={(event) => this.handleOnChangeInput(event)}
+                  value={this.state.address}
+                />
+              </div>
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              className="px-3"
+              color="primary"
+              onClick={() => this.handleAddNewUser()}
+            >
+              Add new
+            </Button>{" "}
+            <Button className="px-3" onClick={this.toggle}>
+              Close
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default ModalUser;
--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -2,17 +2,23 @@ import React, { Component } from "react";
 import { FormattedMessage } from "react-intl";
 import { connect } from "react-redux";
 import "./UserManager.scss";
-import { getAllUsers } from "../../services/userService";
+import { getAllUsers, createNewUserService } from "../../services/userService";
+import ModalUser from "./ModalUser";
 
 class UserManage extends Component {
   constructor(props) {
     super(props);
     this.state = {
       arrUsers: [],
+      isOpenModal: false,
     };
   }
 
   async componentDidMount() {
+    this.getAllUsersFromReact();
+  }
+
+  async getAllUsersFromReact() {
     let response = await getAllUsers("ALL");
     if (response && response.errCode === 0) {
       this.setState({
@@ -21,12 +27,65 @@ class UserManage extends Component {
     }
   }
 
+  // open modal
+  handleAddNewUser = () => {
+    this.setState({
+      isOpenModal: !this.state.isOpenModal,
+    });
+  };
+
+  closeModal = () => {
+    this.setState({
+      isOpenModal: false,
+    });
+  };
+
+  scrollIntoView = (selector) => {
+    let element = document.querySelector(selector);
+    element.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+    });
+  };
+
+  createNewUser = async (childState) => {
+    try {
+      let response = await createNewUserService(childState);
+      if (response && response.errCode !== 0) {
+        alert(response.errMessage);
+      } else {
+        let { arrUsers } = this.state;
+        this.setState({
+          arrUsers: [...arrUsers, childState],
+        });
+        this.closeModal();
+        this.scrollIntoView("table#customers");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   render() {
     let { arrUsers } = this.state;
+    console.log("render");
     return (
       <>
         <div className="user-container">
+          <ModalUser
+            isOpen={this.state.isOpenModal}
+            closeModal={this.closeModal}
+            createNewUser={this.createNewUser}
+          />
           <div className="title text-center">Manager user</div>
+          <div className="mx-1">
+            <button
+              className="btn btn-primary px-3"
+              onClick={() => this.handleAddNewUser()}
+            >
+              <i className="fas fa-plus"></i> Add new users
+            </button>
+          </div>
           <div className="user-table mt-3">
             <table id="customers">
               <thead>
